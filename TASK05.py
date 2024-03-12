59310b74028b7f68001416658d97f0279876898f
sripadhsharma <sripadhsharma68@gmail.com>
2024-03-12 17:35:01 +0530

    loading table from oracle to postgres

--- TASK05.py
+++ TASK05.py
@@ -9,4 +9,4 @@ query = f"SELECT * FROM {'sripadh_0'}"
 df = pd.read_sql(query, oracle_connection_string)
 # Insert data into PostgreSQL
 df.to_sql(name='sripadh_0', con=postgres_connection_string, if_exists="replace", index=False)
-print(f"Data from Oracle table '{'sripadh_0'}' loaded into PostgreSQL.")
+print("Data from Oracle table 'sripadh_0' loaded into PostgreSQL.")
