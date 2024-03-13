abf70b67d4b4361e78e3d2fa44a59ebe1cfef216
sripadhsharma <sripadhsharma68@gmail.com>
2024-03-13 11:10:38 +0530

    loading table from oracle to postgres

--- TASK05.py
+++ TASK05.py
@@ -4,9 +4,10 @@ from sqlalchemy import create_engine
 oracle_connection_string = create_engine('oracle://system:Tippa123@localhost:1521/xe')
 # Create PostgreSQL engine
 postgres_connection_string = create_engine('postgresql://postgres:Tippa123@localhost:5432/postgres')
+table_name='sripadh_0'
 # Fetch data from Oracle
-query = f"SELECT * FROM {'sripadh_0'}"
+query = f"SELECT * FROM {table_name}"
 df = pd.read_sql(query, oracle_connection_string)
 # Insert data into PostgreSQL
 df.to_sql(name='sripadh_0', con=postgres_connection_string, if_exists="replace", index=False)
-print("Data from Oracle table 'sripadh_0' loaded into PostgreSQL.")
+print("Data from Oracle table 'table_name' loaded into PostgreSQL.")
